8d6634b2850e3c17e660f0f838c71f5a0a37f0fc
tristanlenton-cloud <tristan.lenton@gmail.com>
2025-09-18 09:13:00 -0500

    Add files via upload

--- program_5.py
+++ program_5.py
@@ -1,6 +1,19 @@
-# There are two kinds of hot dogs sold:  
-# Hot Dog ($3.50), Chili Dog ($4.50).  
-# Additionally a person can order cheese ($0.50), peppers ($0.75), or grilled onions ($1.00).  
-# Also tax is 7%. 
-# Write a program the inputs the type of hot dog wanted and optional toppings.  
-# The program then displays the hot dog cost, tax and total cost. 
+hotdog = input("Would you like a regular hot dog or a chili dog? ")
+if hotdog == "chili dog":
+    priceOfDog = 4.5
+else:
+    priceOfDog = 3.5
+topping = input("Would you like any topping? ")
+if topping == "yes":
+    topping = input("Which topping would you like? We have cheese, peppers, or grilled onions: ")
+    if topping == "cheese":
+        priceOfTopping = 0.5
+    elif topping == "peppers":
+        priceOfTopping = 0.75
+    elif topping == "grilled onions":
+        priceOfTopping = 1
+    else :
+        priceOfTopping = 0
+print("Hot dog cost: $", priceOfDog)
+print("Tax: $", (priceOfDog + priceOfTopping)*.07)
+print("Total price: $", (priceOfDog + priceOfTopping)*1.07)
